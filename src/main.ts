11dbf67986542fb457587d5730bc488b527b8354
takaodaze <gin2798@gmail.com>
2023-02-10 15:41:58 +0900

    ブロードキャストチャットアプリできた。

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
 import express from "express";
 import http from "http";
 import path from "path";
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
+
+type Message = { msg: string; from: string };
+const createMesasge = (msg: string, from: string): Message => ({
+    msg,
+    from,
+});
 
 app.get("/", (req, res) => {
-    console.log("path.resolve():", path.resolve());
     res.sendFile(path.join(path.resolve(), "public", "index.html"));
+});
+
+io.on("connection", (socket) => {
+    socket.broadcast.emit("hi", createMesasge("hi", socket.id));
+    socket.on("chat message", (msg) => {
+        io.emit("chat message", createMesasge(msg, socket.id));
+    });
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
 });
 
 server.listen(3000, () => {
